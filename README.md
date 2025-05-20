f44ea542bc7a849239a642195dc7bdc2da1436ef
sergioandante <sergioenloscaminos@gmail.com>
2025-05-19 22:44:51 -0500

    Initial commit: estructura inicial del proyecto

---
+++ README.md
@@ -0,0 +1,1 @@
+# Pipeline ETL para Sistema de Ventas 
---
+++ scripts/etl_pipeline.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+ 
+"def main():" 
+"    print('ETL Pipeline para el Laboratorio 2')" 
+ 
+"if __name__ == '__main__':" 
+"    main()" 
